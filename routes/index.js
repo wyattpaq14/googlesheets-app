a22c3c4451ad2a70dc74624eab758a204e2599a8
Wyatt Paquin <paquinwyatt@gmail.com>
2018-01-01 16:42:56 -0500

    some progress

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,141 @@ var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
   process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+/* POST data. */
+router.post('/form', function (req, res, next) {
+  //collect form data to be sent to google sheets
+  var data = {
+    'sunday': req.body.sunday,
+    'monday': req.body.monday,
+    'tuesday': req.body.tuesday,
+    'wednesday': req.body.wednesday,
+    'thursday': req.body.thursday,
+    'friday': req.body.friday,
+    'saturday': req.body.saturday,
+    'week': req.body.week
+  }
+
+
+  // Load client secrets from a local file.
+  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+    authorize(JSON.parse(content), updateTimeSheetInfo);
+  });
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   *
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+      if (err) {
+        getNewToken(oauth2Client, callback);
+      } else {
+        oauth2Client.credentials = JSON.parse(token);
+        callback(oauth2Client);
+      }
+    });
+  }
+
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   *
+   * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback to call with the authorized
+   * client.
+   */
+  function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function (code) {
+      rl.close();
+      oauth2Client.getToken(code, function (err, token) {
+        if (err) {
+          console.log('Error while trying to retrieve access token', err);
+          return;
+        }
+        oauth2Client.credentials = token;
+        storeToken(token);
+        callback(oauth2Client);
+      });
+    });
+  }
+
+  /**
+   * Store token to disk be used in later program executions.
+   *
+   * @param {Object} token The token to store to disk.
+   */
+  function storeToken(token) {
+    try {
+      fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+      if (err.code != 'EEXIST') {
+        throw err;
+      }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+  }
+
+
+
+  function updateTimeSheetInfo(auth) {
+    var body = {
+      "majorDimension": "ROWS",
+      "values": [
+        [ data.week,data.sunday,data.monday,data.tuesday,data.wednesday,data.thursday,data.friday,data.saturday,"" ]
+      ],
+      "range": "A3:I"
+      
+    };
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.append({
+      auth: auth,
+      spreadsheetId: '19hDuT7T45AJSqKORvPC2HESIh7tOzI7MQ8lV2i0DB0E',
+      range: "A3:I",
+      valueInputOption: "RAW",
+      resource: body,
+    }, function (err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+
+    });
+  }
+
+  res.render('result', { pagetitle: 'Wyatts Index Page', data: data });
+
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -26,7 +157,7 @@ router.get('/', function (req, res, next) {
     }
     // Authorize a client with the loaded credentials, then call the
     // Google Sheets API.
-    authorize(JSON.parse(content), listMajors);
+    authorize(JSON.parse(content), getTimeSheetInfo);
   });
 
   /**
@@ -103,13 +234,9 @@ router.get('/', function (req, res, next) {
     console.log('Token stored to ' + TOKEN_PATH);
   }
 
-  /**
-   * Print the names and majors of students in a sample spreadsheet:
-   * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-   */
 
 
-  function listMajors(auth) {
+  function getTimeSheetInfo(auth) {
     var sheets = google.sheets('v4');
     sheets.spreadsheets.values.get({
       auth: auth,
@@ -124,16 +251,9 @@ router.get('/', function (req, res, next) {
       if (rows.length == 0) {
         console.log('No data found.');
       } else {
-        var gData = "";
-        gData += 'Name, Major:';
-        for (var i = 0; i < rows.length; i++) {
-          var row = rows[i];
-          // Print columns A and E, which correspond to indices 0 and 4.
-          gData += row[0] + " " + row[4];
-          console.log('%s, %s', row[0], row[4]);
-        }
-        res.render('index', { pagetitle: 'Wyatts Index Page', data: rows });
+        console.log("We received data!");
       }
+      res.render('index', { pagetitle: 'Wyatts Index Page', data: rows, rowcount: rows.length + 1 });
     });
   }
 
